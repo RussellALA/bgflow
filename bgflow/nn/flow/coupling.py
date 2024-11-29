fix double normalization of spline

bgflow/nn/flow/coupling.py
@@ -180,24 +180,24 @@ class CouplingFlow(Flow):
             )
         self.cat_dim = cat_dim
 
-    def _forward(self, *x, context=None):
+    def _forward(self, *x, context=None, **kwargs):
         input_lengths = [x[i].shape[self.cat_dim] for i in self.transformed_indices]
         inputs = torch.cat([x[i] for i in self.transformed_indices], dim=self.cat_dim)
         cond_inputs = torch.cat([x[i] for i in self.cond_indices], dim=self.cat_dim)
         x = list(x)
-        y, dlogp = self.transformer.forward(cond_inputs, inputs, context=context)
+        y, dlogp = self.transformer.forward(cond_inputs, inputs, context=context, **kwargs)
         y = torch.split(y, input_lengths, self.cat_dim)
         for i, yi in zip(self.transformed_indices, y):
             x[i] = yi
         return (*x, dlogp)
 
-    def _inverse(self, *x, context=None):
+    def _inverse(self, *x, context=None, **kwargs):
         input_lengths = [x[i].shape[self.cat_dim] for i in self.transformed_indices]
         inputs = torch.cat([x[i] for i in self.transformed_indices], dim=self.cat_dim)
         cond_inputs = torch.cat([x[i] for i in self.cond_indices], dim=self.cat_dim)
         x = list(x)
         y, dlogp = self.transformer.forward(
-            cond_inputs, inputs, context=context, inverse=True
+            cond_inputs, inputs, context=context, inverse=True, **kwargs
         )
         y = torch.split(y, input_lengths, self.cat_dim)
         for i, yi in zip(self.transformed_indices, y):

bgflow/nn/flow/transformer/spline.py
@@ -131,7 +131,6 @@ class ConditionalSplineTransformer(Transformer):
         slopes = torch.cat([slopes, slopes[..., [0]]], dim=-1)
         # make noncircular indices non-periodic
         slopes[..., self._noncircular_indices(y_dim), -1] = noncircular_slopes
-        slopes = torch.nn.functional.softplus(slopes, beta=1)
         return widths, heights, slopes
 
     def _forward(self, x, y, context=None):
@@ -150,6 +149,7 @@ class ConditionalSplineTransformer(Transformer):
             top=self._top,
             bottom=self._bottom,
             **self._default_settings,
+            is_normalized=False,
         )
         try:
             z, dlogp = rqs(y)
@@ -181,6 +181,7 @@ class ConditionalSplineTransformer(Transformer):
             top=self._top,
             bottom=self._bottom,
             **self._default_settings,
+            is_normalized=False,
         )
         try:
             z, dlogp = rqs(y)
@@ -228,9 +229,9 @@ class TemperatureSteerableConditionalSplineTransformer(ConditionalSplineTransfor
         widths, heights, slopes = super()._compute_params(x, y_dim)
         widths = torch.nn.functional.softmax(widths, dim=-1)
         heights = torch.nn.functional.softmax(heights, dim=-1)
-        # slopes = torch.nn.functional.softplus(slopes, beta=1)
+        slopes = torch.nn.functional.softplus(slopes, beta=1)
         potential = stable_log(slopes)/beta_0
-        mean_potential = (stable_log(heights) - stable_log(widths))/beta_0
+        mean_potential = (stable_log(heights) + stable_log(widths))/beta_0
         mean_potential_adjusted = beta_1*mean_potential + stable_log(widths)
         log_heights_beta_1 = mean_potential_adjusted - torch.logsumexp(mean_potential_adjusted, dim=-1, keepdim=True)
         log_slopes_beta_1 = beta_1*potential
@@ -251,7 +252,8 @@ class TemperatureSteerableConditionalSplineTransformer(ConditionalSplineTransfor
                 right=self._right,
                 top=self._top,
                 bottom=self._bottom,
-                **self._default_settings
+                **self._default_settings,
+                is_normalized=True
             )
         try:
             z, dlogp = rqs(y)
@@ -281,7 +283,8 @@ class TemperatureSteerableConditionalSplineTransformer(ConditionalSplineTransfor
             right=self._right,
             top=self._top,
             bottom=self._bottom,
-            **self._default_settings
+            **self._default_settings,
+            is_normalized=True
         )
         try:
             z, dlogp = rqs(y)